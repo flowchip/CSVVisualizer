1e2918936de78af9e78003f2b7d13a031c2046c0
Cojan Ciprian <cc.cojan@gmail.com>
2021-07-19 17:23:27 +0200

    Implement fetchData() for StandardCSVReaderService

--- CSVVisualizer/Services/CSVReaderService.swift
+++ CSVVisualizer/Services/CSVReaderService.swift
@@ -8,7 +8,7 @@
 import RxSwift
 
 protocol CSVReaderService {
-    func fetchData() -> Single<Result<[String], Error>>
+    func fetchData() -> Single<Result<Issues, Error>>
 }
 
 protocol CSVReaderServiceProvider {
@@ -16,7 +16,49 @@ protocol CSVReaderServiceProvider {
 }
 
 final class StandardCSVReaderService: CSVReaderService {
-    func fetchData() -> Single<Result<[String], Error>> {
-        return .just(.success([]))
+    let queue = SerialDispatchQueueScheduler(qos: .userInitiated)
+    
+    func fetchData() -> Single<Result<Issues, Error>> {
+        return Single.create { single in
+            do {
+                
+                let filepath = Bundle.main.path(forResource: "issues", ofType: "csv") ?? ""
+                let content = try String(contentsOfFile: filepath)
+                let parsedCSV: [[String]] = content
+                    .components(separatedBy: "\r\n")
+                    .map { $0.components(separatedBy: ",") }
+                
+                let items = Array(parsedCSV.suffix(parsedCSV.count - 1))
+                
+                let issues = Issues(
+                    headers: parsedCSV.first ?? [],
+                    items: items.map { array in
+                        return Issues.Issue(
+                            name: array[safe: 0],
+                            surname: array[safe: 1],
+                            issuesCount: array[safe: 2],
+                            dateOfBirth: array[safe: 3])
+                        })
+
+                single(.success(.success(issues)))
+            } catch {
+                single(.success(.failure(error)))
+            }
+            
+            return Disposables.create()
+        }
+        .subscribe(on: queue)
+    }
+}
+
+struct Issues {
+    let headers: [String]
+    let items: [Issue]
+    
+    struct Issue {
+        let name: String?
+        let surname: String?
+        let issuesCount: String?
+        let dateOfBirth: String?
     }
 }
--- CSVVisualizer/Utilities/Extensions/Array.swift
+++ CSVVisualizer/Utilities/Extensions/Array.swift
@@ -11,14 +11,4 @@ extension Array {
     subscript(safe index: Int) -> Element? {
         return (Int(index) >= 0 && Int(index) < count) ? self[Int(index)] : nil
     }
-
-//    mutating func safeRemove(at index: Int) {
-//        guard index >= 0, index < count else { return }
-//        remove(at: index)
-//    }
-//
-//    mutating func safeInsert(_ newElement: Element, at position: Int) {
-//        guard position >= 0, position <= count else { return }
-//        insert(newElement, at: position)
-//    }
 }
